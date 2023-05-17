012406637a649e741ebe05ef38b96f81328528f2
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-05-17 14:36:52 +0200

    getting all tasks and deleting implemented

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Task} from '../../Task';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
@@ -10,7 +10,14 @@ import {faTimes} from '@fortawesome/free-solid-svg-icons';
 export class TaskItemComponent {
 
   @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  ngOnInit() : void {}
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs'
+import {Observable} from 'rxjs'
 import {Task} from '../Task';
-import {TASKS} from '../mock-tasks';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:8080/tasks/all'
+  private apiUrl = 'http://localhost:8080/tasks'
 
   constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    // const tasks = of(TASKS);
-    // return tasks;
-    return this.http.get<Task[]>(this.apiUrl)
+    const url = `${this.apiUrl}/all`;
+    return this.http.get<Task[]>(url)
+  }
+
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}?taskId=${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
