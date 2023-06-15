a4bf2eec6c56c24f394fec8cf0f98b375e959305
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-06-15 12:43:02 +0200

    a few minor changes

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {Task} from '../../Task';
-import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, HostListener } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
   isExpanded: boolean = false;
   @Input() task!: Task;
   @Output() onEditTask: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
-  constructor() {}
+  constructor(private elementRef: ElementRef) { }
 
   onEdit(task: Task) {
     this.onEditTask.emit(task);
   }
 
   toggleExpand() {
-          this.isExpanded = !this.isExpanded;
-      }
+    this.isExpanded = !this.isExpanded;
+  }
 
-  ngOnInit() : void {}
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isExpanded = false;
+    }
+  }
+
+  ngOnInit(): void { }
 }
-
-
