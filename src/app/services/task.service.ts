fff9cbd35badfa8d3b1cc9f38ba6d25f6348ecd5
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-06-22 16:15:45 +0200

    access to tasks/all still forbidden (403) despite headers with token added to http request in getTasks()

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs'
 import {Task} from '../Task';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { LocalStorageService } from './local-storage.service';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -15,12 +16,14 @@ const httpOptions = {
 export class TaskService {
   private apiUrl = 'http://localhost:8080/tasks'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private localStorage: LocalStorageService) { }
 
   getTasks(): Observable<Task[]> {
-    const url = `${this.apiUrl}/all`;
-    return this.http.get<Task[]>(url)
-  }
+      const url = `${this.apiUrl}/all`;
+      const headers = this.createHeaders();
+
+      return this.http.get<Task[]>(url, { headers });
+    }
 
   deleteTask(task: Task): Observable<void> {
       const url = `${this.apiUrl}?taskId=${task.id}`;
@@ -35,4 +38,14 @@ export class TaskService {
   editTask(task: Task): Observable<Task> {
     return this.http.put<Task>(this.apiUrl, task, httpOptions);
   }
+
+  private createHeaders(): HttpHeaders {
+      const token = this.localStorage.get('jwt-token');
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
+      return headers;
+    }
+
 }
