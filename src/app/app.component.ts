8983ec9eeec20b0dccfa1fef8cf9d4c070f770a4
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-06-23 12:43:41 +0200

    token expiration validation for loggedIn added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,38 @@
-import { OnInit, Component, Input } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
 import { LocalStorageService } from "./services/local-storage.service";
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   logged!: boolean;
 
   constructor(private localStorage: LocalStorageService) { }
 
   ngOnInit(): void {
     const token = this.localStorage.get('jwt-token');
-    if(token !== null) {
+    if (token !== null) {
+      const decodedToken: any = jwt_decode(token);
+      if (decodedToken.exp * 1000 > Date.now()) {
         this.logged = true;
+      } else {
+        this.localStorage.remove('jwt-token');
+      }
     }
   }
 
   handleLogin(value: boolean) {
-      this.logged = value;
+    this.logged = value;
   }
 
   handleLogout() {
     this.localStorage.remove('jwt-token');
 
-    if (this.localStorage.get('jwt-token') == null ) {
+    if (this.localStorage.get('jwt-token') == null) {
       this.logged = !this.logged;
     }
-
   }
 }
