9459136805d59159fe1654891cddbfa420fe2e05
pmajerowski <105345146+pmajerowski@users.noreply.github.com>
2023-06-30 15:50:11 +0200

    login error information display added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,6 +10,7 @@ import { LocalStorageService } from "../../services/local-storage.service";
 export class LoginComponent {
   email !: string;
   password !: string;
+  error: string = ''
   @Output() loggedIn = new EventEmitter<boolean>();
 
   constructor(private loginService: LoginServiceService, private localStorage: LocalStorageService) { }
@@ -17,11 +18,20 @@ export class LoginComponent {
   onSubmit(event: Event) {
       event.preventDefault();
       this.loginService.signIn(this.email, this.password)
-        .subscribe((response) => {
-              this.localStorage.set('jwt-token', response.token);
-              this.loggedIn.emit(true);
-          });
-  }
+        .subscribe(
+          (response) => {
+            this.localStorage.set('jwt-token', response.token);
+            this.loggedIn.emit(true);
+          },
+          (error) => {
+            if (error.status === 403) {
+              this.error = 'Invalid email or password.'; // Update the error message
+            } else {
+              this.error = 'An error occurred. Please try again.'; // Generic error message
+            }
+          }
+        );
+    }
 
   toggleRegister() {
 
